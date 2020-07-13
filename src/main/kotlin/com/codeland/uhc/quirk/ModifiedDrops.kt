3a459ca2943677527ec659bb084415a13cc5cb22
gatcuddy <emmettglaser@gmail.com>
2020-07-13 10:16:05 -0700

    going to stephen's questionable drops

--- src/main/kotlin/com/codeland/uhc/quirk/ModifiedDrops.kt
+++ src/main/kotlin/com/codeland/uhc/quirk/ModifiedDrops.kt
@@ -1,6 +1,8 @@
 package com.codeland.uhc.quirk
 
 import net.md_5.bungee.api.ChatColor
+import org.bukkit.Color
+import org.bukkit.FireworkEffect
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Entity
@@ -8,26 +10,36 @@ import org.bukkit.entity.EntityType
 import org.bukkit.entity.Item
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.Damageable
+import org.bukkit.inventory.meta.FireworkEffectMeta
 import org.bukkit.inventory.meta.ItemMeta
 
 object ModifiedDrops {
 	fun randomEnchantedBook(): ItemStack {
-		val ret = ItemStack(Material.BOOK)
+		val ret = ItemStack(Material.ENCHANTED_BOOK)
 
 		val meta = ret.itemMeta
 
-		val enchant = Enchantment.values()[(Math.random() * Enchantment.values().size).toInt()]
-		meta.addEnchant(enchant, (Math.random() * (enchant.maxLevel + 1)).toInt(), true)
+		val enchant = Enchantment.values()[randRange(0, Enchantment.values().size - 1)]
+		meta.addEnchant(enchant, randRange(1, enchant.maxLevel), true)
 
 		ret.itemMeta = meta
 
 		return ret
 	}
 
+	fun fireworkStar(amount: Int): ItemStack {
+		val stack = ItemStack(Material.FIREWORK_STAR, amount)
+		val meta = stack.itemMeta as FireworkEffectMeta
+		meta.effect = FireworkEffect.builder().withColor(Color.LIME).build()
+		stack.itemMeta = meta
+
+		return stack
+	}
+
 	fun randomDamagedItem(type: Material): ItemStack {
 		val ret = ItemStack(type)
 		val damageable = ret.itemMeta as Damageable
-		damageable.damage = (type.maxDurability * Math.random()).toInt()
+		damageable.damage = randRange(0, type.maxDurability.toInt())
 		ret.itemMeta = damageable as ItemMeta
 
 		return ret
@@ -69,13 +81,21 @@ object ModifiedDrops {
 
 				drops.add(when {
 					rand < 0.25 -> ItemStack(Material.TNT, amount)
-					rand < 0.5 -> ItemStack(Material.FIREWORK_STAR, amount)
-					else -> ItemStack(Material.GUNPOWDER, amount)
+					rand < 0.5 -> fireworkStar(amount * 2)
+					else -> ItemStack(Material.GUNPOWDER, amount * 2)
 				})
 			}
 
 			EntityType.PHANTOM -> {
-				drops.add(randomDamagedItem(Material.ELYTRA))
+				if (Math.random() < 0.6) {
+					val elytra = randomDamagedItem(Material.ELYTRA)
+
+					addRandomEnchants(elytra, arrayOf(
+							Enchantment.DURABILITY
+					), 0.333f)
+
+					drops.add(elytra)
+				}
 			}
 
 			EntityType.ZOMBIE, EntityType.HUSK, EntityType.ZOMBIE_VILLAGER -> {
@@ -83,7 +103,14 @@ object ModifiedDrops {
 					for (i in 0..30)
 						drops.add(namedItem(Material.CARROT, "${ChatColor.GOLD}${ChatColor.BOLD}Carrot Warrior #${randRange(0, Int.MAX_VALUE - 1)}"))
 				else
-					drops.add(ItemStack(Material.CARROT, randRange(1, 4)))
+					drops.add(ItemStack(Material.CARROT))
+
+				if (Math.random() < 0.15) {
+					val egg = Summoner.getSpawnEgg(type, true, false)
+
+					if (egg != null)
+						drops.add(ItemStack(egg))
+				}
 			}
 
 			EntityType.SKELETON, EntityType.STRAY -> {
@@ -99,10 +126,13 @@ object ModifiedDrops {
 			}
 
 			EntityType.SPIDER, EntityType.CAVE_SPIDER, EntityType.SILVERFISH -> {
-				drops.add(ItemStack(Material.PAPER, randRange(1, 4)))
+				val amount = randRange(0, 3)
 
-				if (Math.random() < 0.05)
-					drops.add(randomEnchantedBook())
+				if (amount > 0)
+					drops.add(ItemStack(Material.PAPER, amount))
+
+				//if (Math.random() < 0.04)
+				//	drops.add(randomEnchantedBook())
 			}
 
 			EntityType.DROWNED -> {
@@ -110,10 +140,10 @@ object ModifiedDrops {
 
 				addRandomEnchants(trident, arrayOf(
 					Enchantment.RIPTIDE,
-					Enchantment.CHANNELING,
-					Enchantment.LOYALTY,
-					Enchantment.IMPALING
-				), 0.5f)
+					//Enchantment.CHANNELING,
+					Enchantment.LOYALTY
+					//Enchantment.IMPALING
+				), 0.6f)
 
 				drops.add(trident)
 			}
