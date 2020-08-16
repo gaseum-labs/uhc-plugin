1b4f1994ee8af29b09ea8288f4558e7b1f2d2384
acou12 <51276518+acou12@users.noreply.github.com>
2020-08-15 21:57:55 -0600

    Add shared inventory

--- src/main/kotlin/com/codeland/uhc/quirk/SharedInventory.kt
+++ src/main/kotlin/com/codeland/uhc/quirk/SharedInventory.kt
@@ -1,17 +1,30 @@
 package com.codeland.uhc.quirk;
 
+import com.codeland.uhc.core.GameRunner
 import org.bukkit.Bukkit
 import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
 
 class SharedInventory(type: QuirkType) : Quirk(type) {
 	companion object {
-		lateinit var inventory: Inventory
+		lateinit var contents: Array<ItemStack>
 	}
 
 	override fun onEnable() {
-		inventory = Bukkit.createInventory(null, InventoryType.PLAYER)
-		inventory.clear()
+		GameRunner.plugin.server.scheduler.scheduleSyncRepeatingTask(GameRunner.plugin, Runnable {
+			for (player in Bukkit.getServer().onlinePlayers) {
+				val playersContents = player.inventory.contents
+				if (!playersContents.contentEquals(contents)) {
+					contents = playersContents.copyOf()
+					for (other in Bukkit.getServer().onlinePlayers) {
+						if (other != player) other.inventory.contents = playersContents
+					}
+					// this sucks because it'll override any other inventory changes
+					// todo: fix
+				}
+			}
+		}, 1, 1)
 	}
 
 	override fun onDisable() {
