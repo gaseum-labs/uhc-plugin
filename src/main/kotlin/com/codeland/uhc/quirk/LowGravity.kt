c41b5fa55a63a1b909adc2e17a3c7ccec9476460
acou12 <51276518+acou12@users.noreply.github.com>
2020-08-20 16:52:47 -0600

    Minor changes to Low Gravity
    Including disabling fall damage and fixing caves.

--- src/main/kotlin/com/codeland/uhc/quirk/LowGravity.kt
+++ src/main/kotlin/com/codeland/uhc/quirk/LowGravity.kt
@@ -2,9 +2,12 @@ package com.codeland.uhc.quirk
 
 import com.codeland.uhc.core.GameRunner
 import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.Material.*
 import org.bukkit.util.Vector
+import java.util.*
+import kotlin.collections.HashMap
 
 
 class LowGravity(type: QuirkType) : Quirk(type) {
@@ -14,7 +17,7 @@ class LowGravity(type: QuirkType) : Quirk(type) {
         private const val GRAVITY: Double = 0.5
         // these are blocks that should be counted as 'air'
         private val airBlocks = listOf<Material>(
-            AIR, GRASS, TALL_GRASS, WHEAT_SEEDS,
+                AIR, CAVE_AIR, GRASS, TALL_GRASS, WHEAT_SEEDS,
                 BEETROOT_SEEDS, MELON_SEEDS, PUMPKIN_SEEDS
         )
     }
@@ -25,13 +28,13 @@ class LowGravity(type: QuirkType) : Quirk(type) {
                 if (player.world.getBlockAt(player.location.subtract(0.0, 0.01, 0.0)).type in airBlocks) {
                     // - G = 0.08 for living entities
                     // https://minecraft.gamepedia.com/Entity#Motion_of_entities
+                    player.velocity = Vector(player.velocity.x, player.velocity.y - GRAVITY * 0.08 + 0.08, player.velocity.z)
                     // - also give them a little push in the direction they're facing
-                    // to counteract air resistance
-                    // - feels unnatural sometimes. for example, when walking backwards
-                    // and jumping you move forwards
-                    // - can't really think of any other way to do this
-                    val move = player.location.direction.normalize().multiply(0.03)
-                    player.velocity = Vector(player.velocity.x + move.x, player.velocity.y - GRAVITY * 0.08 + 0.08, player.velocity.z + move.z)
+                    // to counteract air resistance (if they're sprinting)
+                    if (player.isSprinting) {
+                        val direction = player.location.direction
+                        player.velocity = Vector(player.velocity.x + direction.x * 0.03, player.velocity.y, player.velocity.z + direction.z * 0.03)
+                    }
                 }
             }
         }, 1, 1)
