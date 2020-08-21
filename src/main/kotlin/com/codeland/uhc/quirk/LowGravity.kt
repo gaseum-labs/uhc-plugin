5d86b9adaf19ae706d1aa8b2993d7fee2d7e6eb0
acou12 <51276518+acou12@users.noreply.github.com>
2020-08-20 20:01:47 -0600

    Spectators are no longer affected by upward push

--- src/main/kotlin/com/codeland/uhc/quirk/LowGravity.kt
+++ src/main/kotlin/com/codeland/uhc/quirk/LowGravity.kt
@@ -2,6 +2,7 @@ package com.codeland.uhc.quirk
 
 import com.codeland.uhc.core.GameRunner
 import org.bukkit.Bukkit
+import org.bukkit.GameMode
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.Material.*
@@ -15,17 +16,13 @@ class LowGravity(type: QuirkType) : Quirk(type) {
     companion object {
         var taskId: Int = 0
         private const val GRAVITY: Double = 0.5
-        // these are blocks that should be counted as 'air'
-        private val airBlocks = listOf<Material>(
-                AIR, CAVE_AIR, GRASS, TALL_GRASS, WHEAT_SEEDS,
-                BEETROOT_SEEDS, MELON_SEEDS, PUMPKIN_SEEDS
-        )
     }
 
     override fun onEnable() {
         taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(GameRunner.plugin, {
             for (player in Bukkit.getOnlinePlayers()) {
-                if (player.world.getBlockAt(player.location.subtract(0.0, 0.01, 0.0)).type in airBlocks) {
+                if (!player.world.getBlockAt(player.location.subtract(0.0, 0.01, 0.0)).type.isSolid
+                        && player.gameMode == GameMode.SURVIVAL) {
                     // - G = 0.08 for living entities
                     // https://minecraft.gamepedia.com/Entity#Motion_of_entities
                     player.velocity = Vector(player.velocity.x, player.velocity.y - GRAVITY * 0.08 + 0.08, player.velocity.z)
