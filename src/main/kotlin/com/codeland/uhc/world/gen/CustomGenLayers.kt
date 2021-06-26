d11b25eea141c7fcbadee59ed019616de64e8056
EmmettGlaser <emmettglaser@gmail.com>
2021-06-25 22:41:22 -0700

    better river gen

--- src/main/kotlin/com/codeland/uhc/world/gen/CustomGenLayers.kt
+++ src/main/kotlin/com/codeland/uhc/world/gen/CustomGenLayers.kt
@@ -56,38 +56,38 @@ object CustomGenLayers {
 	private fun <T : Area, C : AreaContextTransformed<T>> createAreaFactoryGame(
 		seed: LongFunction<C>,
 	): AreaFactory<T> {
-		var baseLayer = LayerTemperature().a(seed.apply(12L))
+		var baseLayer = LayerTemperature().a(seed.apply(1000L))
 
 		baseLayer = GenLayerZoom.a.a(seed.apply(1001L), baseLayer)
 
-		baseLayer = GenLayerMerge().a(seed.apply(49L), baseLayer)
-		baseLayer = GenLayerIdBiome().a(seed.apply(3333L), baseLayer)
+		baseLayer = GenLayerMerge().a(seed.apply(2L), baseLayer)
+		baseLayer = GenLayerIdBiome().a(seed.apply(3L), baseLayer)
 
 		baseLayer = GenLayerZoom.a.a(seed.apply(1002L), baseLayer)
 
-		baseLayer = GenLayerSpecial().a(seed.apply(192L), baseLayer)
+		baseLayer = GenLayerSpecial().a(seed.apply(4L), baseLayer)
 
 		baseLayer = GenLayerZoom.a.a(seed.apply(1003L), baseLayer)
 
-		var hillLayer = LayerNoise().a(seed.apply(88L))
-		hillLayer = GenLayerZoom.a.a(seed.apply(90L), hillLayer)
-		hillLayer = GenLayerZoom.a.a(seed.apply(90L), hillLayer)
-		hillLayer = GenLayerEdge(1).a(seed.apply(142L), hillLayer)
-		hillLayer = GenLayerZoom.a.a(seed.apply(2920L), hillLayer)
+		var hillLayer = LayerNoise().a(seed.apply(5L))
+		hillLayer = GenLayerZoom.a.a(seed.apply(6L), hillLayer)
+		hillLayer = GenLayerZoom.a.a(seed.apply(7L), hillLayer)
+		hillLayer = GenLayerEdge(1).a(seed.apply(8L), hillLayer)
+		hillLayer = GenLayerZoom.a.a(seed.apply(9L), hillLayer)
 
-		baseLayer = GenLayerHillApply().a(seed.apply(23L), baseLayer, hillLayer)
-		baseLayer = GenLayerBiomeEdge().a(seed.apply(292L), baseLayer)
+		baseLayer = GenLayerHillApply().a(seed.apply(10L), baseLayer, hillLayer)
+		baseLayer = GenLayerBiomeEdge().a(seed.apply(11L), baseLayer)
 
 		baseLayer = GenLayerZoom.a.a(seed.apply(1004L), baseLayer)
 		baseLayer = GenLayerZoom.a.a(seed.apply(1005L), baseLayer)
 
-		var riverLayer = LayerNoise().a(seed.apply(213L))
+		var riverLayer = LayerNoise().a(seed.apply(1000L))
 		riverLayer = GenLayerZoom.a.a(seed.apply(1001L), riverLayer)
 		riverLayer = GenLayerZoom.a.a(seed.apply(1002L), riverLayer)
 		riverLayer = GenLayerZoom.a.a(seed.apply(1003L), riverLayer)
 		riverLayer = GenLayerZoom.a.a(seed.apply(1004L), riverLayer)
-		riverLayer = GenLayerEdge(BiomeNo.RIVER).a(seed.apply(122L), riverLayer)
 		riverLayer = GenLayerZoom.a.a(seed.apply(1005L), riverLayer)
+		riverLayer = GenLayerEdge(BiomeNo.RIVER).a(seed.apply(122L), riverLayer)
 
 		baseLayer = GenLayerRiverApply().a(seed.apply(2220L), baseLayer, riverLayer)
 
@@ -95,18 +95,14 @@ object CustomGenLayers {
 	}
 
 	fun createGenLayerNether(seed: Long): GenLayer {
-		val var6 = createAreaFactoryNether(LongFunction { var2x: Long -> WorldGenContextArea(25, seed, var2x) })
-		return GenLayer(var6)
+		return GenLayer(createAreaFactoryNether { WorldGenContextArea(25, seed, it) })
 	}
 
 	fun createGenLayerLobby(seed: Long): GenLayer {
-		val var6 = createAreaFactoryLobby(LongFunction { var2x: Long -> WorldGenContextArea(25, seed, var2x) })
-		return GenLayer(var6)
+		return GenLayer(createAreaFactoryLobby { WorldGenContextArea(25, seed, it) })
 	}
 
 	fun createGenLayerGame(seed: Long): AreaLazy {
-		return createAreaFactoryGame(
-			LongFunction { var2x: Long -> WorldGenContextArea(25, seed, var2x) }
-		).make()
+		return createAreaFactoryGame { WorldGenContextArea(25, seed, it) }.make()
 	}
 }
--- src/main/kotlin/com/codeland/uhc/world/gen/layer/game/GenLayerEdge.kt
+++ src/main/kotlin/com/codeland/uhc/world/gen/layer/game/GenLayerEdge.kt
@@ -5,12 +5,8 @@ import net.minecraft.world.level.newbiome.layer.traits.AreaTransformer4
 
 class GenLayerEdge(val produce: Int) : AreaTransformer4 {
 	override fun a(context: WorldGenContext, p1: Int, p2: Int, p3: Int, p4: Int, p5: Int): Int {
-		return if (p5 == 0) {
-			if (p1 == 1 || p2 == 1 || p3 == 1 || p4 == 1) {
-				produce
-			} else {
-				0
-			}
+		return if (p1 != p5 || p2 != p5 || p3 != p5 || p4 != p5) {
+			produce
 		} else {
 			0
 		}
