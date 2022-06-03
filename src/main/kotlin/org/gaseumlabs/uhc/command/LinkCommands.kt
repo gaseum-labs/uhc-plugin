ebf996693adb7429dd9daa795aafaeb2bd4d857b
acou12 <notarealemailaddress@fake.com>
2022-06-03 11:46:15 -0400

    Begin link auth

--- src/main/kotlin/org/gaseumlabs/uhc/command/LinkCommands.kt
+++ src/main/kotlin/org/gaseumlabs/uhc/command/LinkCommands.kt
@@ -2,6 +2,7 @@ package org.gaseumlabs.uhc.command
 
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.*
+import com.google.gson.JsonObject
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.gaseumlabs.uhc.core.UHC
@@ -10,12 +11,20 @@ import org.gaseumlabs.uhc.util.Action
 
 class LinkCommands : BaseCommand() {
 	@CommandAlias("link")
-	@Description("enter the code you received from the website")
+	@Description("use to link your minecraft account with your uhcsaturday account")
 	fun linkCommand(sender: CommandSender, code: String) {
 		val player = sender as? Player ?: return
 
-		UHC.dataManager.verifyCode(player, code).thenAccept {
-			Action.sendGameMessage(player, "Successfully linked your minecraft account")
+		val body = JsonObject()
+		body.addProperty("uuid", player.uniqueId.toString())
+		body.addProperty("username", player.name)
+		val response = UHC.dataManager.postRequest(
+			"api/bot/createVerifyLink",
+			body
+		).thenAccept {
+			player.sendMessage(it.body())
+			val link = it.body().split("\"")[3]
+			player.sendMessage("Navigate to the following link to continue: $link")
 		}.exceptionally { ex ->
 			when (ex) {
 				is OfflineException -> Commands.errorMessage(player, "The server is in offline mode")
