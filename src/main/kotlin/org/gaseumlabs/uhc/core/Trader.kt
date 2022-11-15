c15f0cf9b8ffe5a553fadb7892df347acbfdbd8a
balduvian <emmettglaser@gmail.com>
2022-11-14 23:26:30 -0800

    wander trader trades buff

--- src/main/kotlin/org/gaseumlabs/uhc/core/Trader.kt
+++ src/main/kotlin/org/gaseumlabs/uhc/core/Trader.kt
@@ -28,17 +28,19 @@ class Trader {
 		) as WanderingTrader
 
 		trader.recipes = listOf(
-			createTrade(1, ItemStack(OXEYE_DAISY), 5),
-			createTrade(1, ItemStack(SAND, 9), 5),
-			createTrade(1, ItemStack(GLOWSTONE_DUST, 8), 5),
-			createTrade(1, ItemStack(SUGAR_CANE, 3), 5),
-			createTrade(1, ItemStack(LEATHER), 5),
-			createTrade(2, ItemStack(APPLE), 4),
-			createTrade(3, ItemStack(MELON_SLICE), 3),
-			createTrade(1, ItemStack(PUFFERFISH_BUCKET), 5),
-			createTrade(1, ItemStack(FEATHER), 5),
-			createTrade(1, ItemStack(EXPERIENCE_BOTTLE), 5),
-			createTrade(2, ItemStack(ENDER_PEARL), 4),
+			createTrade(1, ItemStack(OXEYE_DAISY), 6),
+			createTrade(1, ItemStack(SAND, 9), 6),
+			createTrade(1, ItemStack(GLOWSTONE_DUST, 8), 6),
+			createTrade(1, ItemStack(SUGAR_CANE, 3), 6),
+			createTrade(1, ItemStack(LEATHER), 6),
+			createTrade(1, ItemStack(PUFFERFISH_BUCKET), 6),
+			createTrade(1, ItemStack(FEATHER), 6),
+			createTrade(1, ItemStack(EXPERIENCE_BOTTLE), 6),
+			createTrade(2, ItemStack(APPLE), 6),
+			createTrade(2, ItemStack(GOLD_INGOT, 2), 6),
+			createTrade(2, ItemStack(ENDER_PEARL), 6),
+			createTrade(3, ItemStack(MELON_SLICE), 6),
+			createTrade(4, ItemStack(DIAMOND), 6),
 		)
 
 		return trader
